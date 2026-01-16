756a2800477335fe0ae2cafda173daf407aeed52
mandip765 <135602461+mandip765@users.noreply.github.com>
2026-01-17 00:46:42 +0100

    Add URL path for adding an item

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import dashboard
+from .views import dashboard, add_item
 
 urlpatterns = [
     path('', dashboard, name='dashboard'),
+    path('add/', add_item, name='add_item'),
 ]
