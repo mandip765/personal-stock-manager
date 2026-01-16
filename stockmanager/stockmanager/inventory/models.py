54f4d1e8bedba4664307f8d0bf5db0d2e87f1ad1
mandip765 <135602461+mandip765@users.noreply.github.com>
2026-01-16 23:59:19 +0100

    Delete stockmanager/stockmanager/inventory/models.py

--- stockmanager/stockmanager/inventory/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Item(models.Model):
-    name = models.CharField(max_length=200)
-    quantity = models.IntegerField(default=0)
-    low_stock = models.IntegerField(default=5)
-
-    def __str__(self):
-        return self.name
