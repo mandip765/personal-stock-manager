d56feafb4faeb61e4eb8e018bbe6fea34eb9d62c
mandip765 <135602461+mandip765@users.noreply.github.com>
2026-01-17 00:11:30 +0100

    Delete inventory/views.py

--- inventory/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, redirect
-from .models import Item
-
-def dashboard(request):
-    items = Item.objects.all()
-    low = items.filter(quantity__lte=models.F('low_stock'))
-    return render(request, 'inventory/dashboard.html', {'items': items, 'low': low})
