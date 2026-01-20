0f33093e6c1d15fef3d048d515e1c83e4ed7fb5c
mandip765 <135602461+mandip765@users.noreply.github.com>
2026-01-20 10:24:35 +0100

    Update urls.py

--- inventory/urls.py
+++ inventory/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
-    path('add/', views.add_item, name='add_item'),
-    path('scan/', views.upload_bill, name='upload_bill'),
+    path('', views.dashboard),
+    path('add/', views.add_item),
+    path('bill/', views.upload_bill),
 ]
